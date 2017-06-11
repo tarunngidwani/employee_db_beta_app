35751970fb2e0e6a1d9a9dda9342cbd31e776cb1
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-11 13:51:08 -0500

    Add validation, tests, fixture data: state name
      - Validation, tests, and fixture date ensure that
        name of state in State model cannot be nil or empty

--- test/models/state_test.rb
+++ test/models/state_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class StateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'state name cannot be nil' do
+    state = states :state_name_nil_val
+    assert_not_nil_empty state, :name
+  end
+
+  test 'state name cannot be empty' do
+    state = states :state_name_empty_val
+    assert_not_nil_empty state, :name
+  end
+
+  private
+
+  # Helper method:
+  # Asserts that the specified attribute value
+  # for obj under test cannot be nil or empty
+  def assert_not_nil_empty(obj, attr_symbol)
+    assert obj.invalid?
+    assert obj.errors[attr_symbol].any?
+    assert_equal [(I18n.translate 'errors.messages.blank')],
+                 obj.errors[attr_symbol]
+  end
 end
