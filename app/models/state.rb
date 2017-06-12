d4e18359a2cf23bfec63f53e545da78d666f450c
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-12 16:56:45 -0500

    Update broken state name regular expression
      - Account for state names that are made up of
        two words
    
      - Update supporting unit tests for state model to
        verify that the updated regular expression fixes
        prior issues and supports expected results

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,7 @@
 class State < ApplicationRecord
   validates :name, :name_abbr, presence: true
   validates :name, allow_blank: true, format: {
-    with: /\A[A-Z][a-z]+\z/,
+    with: /\A[A-Z][a-z]+( [A-Z][a-z]+)?\z/,
     message: 'must contain letters a to z with the first letter being uppercase'
   }
   validates :name_abbr, allow_blank: true, format: {
--- test/models/state_test.rb
+++ test/models/state_test.rb
@@ -12,19 +12,28 @@ class StateTest < ActiveSupport::TestCase
   end
 
   test 'state name is invalid' do
-    state = states :state_name_invalid
-    assert state.invalid?
-    assert state.errors[:name].any?
-    assert_equal state.errors[:name],
-                 ['must contain letters a to z with the first letter being ' \
+    invalid_states = [
+      states(:state_name_invalid_0), states(:state_name_invalid_1),
+      states(:state_name_invalid_2), states(:state_name_invalid_3),
+      states(:state_name_invalid_4)
+    ]
+    invalid_states.each do |state|
+      assert state.invalid?
+      assert state.errors[:name].any?
+      assert_equal state.errors[:name],
+                   ['must contain letters a to z with the first letter being ' \
                       'uppercase']
+    end
   end
 
   test 'state name is valid' do
-    state = states :state_name_valid
-    assert state.valid?
-    assert_not state.errors[:name].any?
-    assert_equal state.errors[:name], []
+    valid_states = [states(:state_name_valid_0), states(:state_name_valid_1)]
+
+    valid_states.each do |state|
+      assert state.valid?
+      assert_not state.errors[:name].any?
+      assert_equal state.errors[:name], []
+    end
   end
 
   test 'state name abbreviation cannot be nil' do
