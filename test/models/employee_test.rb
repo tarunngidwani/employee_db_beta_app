4716ed8ff44ee6c2d26c70be45fda7816a2cb02e
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-13 17:19:48 -0500

    Remove redundant use of model name in Employee model's unit tests

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 require_relative 'helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  test 'employee name cannot be nil' do
+  test 'name cannot be nil' do
     employee = employees :employee_name_nil_val
     assert_not_nil_empty employee, :name
   end
 
-  test 'employee name cannot be empty' do
+  test 'name cannot be empty' do
     employee = employees :employee_name_empty_val
     assert_not_nil_empty employee, :name
   end
 
-  test 'employee name does not being with an uppercase letter' do
+  test 'name does not being with an uppercase letter' do
     employee = employees :employee_name_not_begins_with_uppercase_letter
     assert employee.invalid?
     assert employee.errors[:name].any?
@@ -20,24 +20,24 @@ class EmployeeTest < ActiveSupport::TestCase
                  ['must begin with an uppercase letter [A-Z]']
   end
 
-  test 'employee name does being with an uppercase letter' do
+  test 'name does being with an uppercase letter' do
     employee = employees :employee_name_valid
     assert employee.valid?
     assert_not employee.errors[:name].any?
     assert_equal employee.errors[:name], []
   end
 
-  test 'employee house number cannot be nil' do
+  test 'house number cannot be nil' do
     employee = employees :employee_house_num_nil_val
     assert_not_nil_empty employee, :house_num
   end
 
-  test 'employee house number cannot be empty' do
+  test 'house number cannot be empty' do
     employee = employees :employee_house_num_empty_val
     assert_not_nil_empty employee, :house_num
   end
 
-  test 'employee house number cannot be a negative value' do
+  test 'house number cannot be a negative value' do
     employee = employees :employee_house_num_negative_val
     assert employee.invalid?
     assert employee.errors[:house_num].any?
@@ -45,7 +45,7 @@ class EmployeeTest < ActiveSupport::TestCase
            ['must be a positive numeric value']
   end
 
-  test 'employee house number cannot be a string' do
+  test 'house number cannot be a string' do
     employee = employees :employee_house_num_string_val
     assert employee.invalid?
     assert employee.errors[:house_num].any?
@@ -53,7 +53,7 @@ class EmployeeTest < ActiveSupport::TestCase
            ['must be a positive numeric value']
   end
 
-  test 'employee house number is a positive numeric value' do
+  test 'house number is a positive numeric value' do
     employee = employees :employee_house_num_valid
     assert employee.valid?
     assert_not employee.errors[:house_num].any?
