39c7a2ba297bd739ff42a5f213e7cbd5ed29cbb2
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-14 11:59:14 -0500

    Move invalid employee fixture data to test cases
        - Remove invalid employee fixture data from employee
          fixtures file
        - Move the employee test data to the test cases within
          models/employee_test.rb
    
    Note: Fixture data needs to be valid as it is used by the controller
          functional tests as seed data in the test database while
          executing the various funcitonal test cases

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -3,17 +3,27 @@ require_relative 'helper'
 
 class EmployeeTest < ActiveSupport::TestCase
   test 'name cannot be nil' do
-    employee = employees :employee_name_nil_val
+    employee = Employee.new(name: nil, house_num: 123, street_name: 'Book Rd',
+                            city: 'Naperville', state:
+                            (states :state_name_valid_0), zip_code: 60_564)
+
     assert_not_nil_empty employee, :name
   end
 
   test 'name cannot be empty' do
-    employee = employees :employee_name_empty_val
+    employee = Employee.new(name: '', house_num: 432, street_name: 'Park Blvd',
+                            city: 'Harlem', state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert_not_nil_empty employee, :name
   end
 
   test 'name does not being with an uppercase letter' do
-    employee = employees :employee_name_not_begins_with_uppercase_letter
+    employee = Employee.new(name: 'karl stolley', house_num: 342,
+                            street_name: 'Link Blvd', city: 'Blemont',
+                            state: (states :state_name_valid_0),
+                            zip_code: 60_614)
+
     assert employee.invalid?
     assert employee.errors[:name].any?
     assert_equal employee.errors[:name],
@@ -28,17 +38,28 @@ class EmployeeTest < ActiveSupport::TestCase
   end
 
   test 'house number cannot be nil' do
-    employee = employees :employee_house_num_nil_val
+    employee = Employee.new(name: 'Test Name', house_num: nil,
+                            street_name: 'Book Rd', city: 'Naperville',
+                            state: (states :state_name_valid_0),
+                            zip_code: 60_564)
+
     assert_not_nil_empty employee, :house_num
   end
 
   test 'house number cannot be empty' do
-    employee = employees :employee_house_num_empty_val
+    employee = Employee.new(name: 'Fail Test', house_num: '', street_name: 'Park Blvd',
+                            city: 'Harlem', state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert_not_nil_empty employee, :house_num
   end
 
   test 'house number cannot be a negative value' do
-    employee = employees :employee_house_num_negative_val
+    employee = Employee.new(name: 'Test Fail', house_num: -122,
+                            street_name: 'Park Blvd', city: 'Harlem',
+                            state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert employee.invalid?
     assert employee.errors[:house_num].any?
     assert employee.errors[:house_num],
@@ -46,7 +67,11 @@ class EmployeeTest < ActiveSupport::TestCase
   end
 
   test 'house number cannot be a string' do
-    employee = employees :employee_house_num_string_val
+    employee = Employee.new(name: 'Test Fail', house_num: 'T122est',
+                            street_name: 'Park Blvd', city: 'Harlem',
+                            state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert employee.invalid?
     assert employee.errors[:house_num].any?
     assert employee.errors[:house_num],
@@ -61,27 +86,45 @@ class EmployeeTest < ActiveSupport::TestCase
   end
 
   test 'street name cannot be nil' do
-    employee = employees :employee_street_name_nil_val
+    employee = Employee.new(name: 'Test Fail', house_num: 122, street_name: nil,
+                            city: 'Harlem', state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert_not_nil_empty employee, :street_name
   end
 
   test 'street name cannot be empty' do
-    employee = employees :employee_street_name_empty_val
+    employee = Employee.new(name: 'Test Fail', house_num: 122, street_name: '',
+                            city: 'Harlem', state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert_not_nil_empty employee, :street_name
   end
 
   test 'city cannot be nil' do
-    employee = employees :employee_city_nil_val
+    employee = Employee.new(name: 'Test Fail', house_num: 122,
+                            street_name: 'Filestream Land', city: nil,
+                            state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert_not_nil_empty employee, :city
   end
 
   test 'city cannot be empty' do
-    employee = employees :employee_city_empty_val
+    employee = Employee.new(name: 'Test Fail', house_num: 122,
+                            street_name: 'File Handle', city: '',
+                            state: (states :state_name_valid_0),
+                            zip_code: 60_616)
+
     assert_not_nil_empty employee, :city
   end
 
   test 'city does not being with an uppercase letter' do
-    employee = employees :employee_city_not_begins_with_uppercase_letter
+    employee = Employee.new(name: 'Karl Stolley', house_num: 879,
+                            street_name: 'Bleek Blvd', city: 'mt. prospect',
+                            state: (states :state_name_valid_1),
+                            zip_code: 60_645)
+
     assert employee.invalid?
     assert employee.errors[:city].any?
     assert_equal employee.errors[:city],
@@ -96,19 +139,27 @@ class EmployeeTest < ActiveSupport::TestCase
   end
 
   test 'state_id cannot be nil' do
-    employee = employees :employee_state_nil_val
+    employee = Employee.new(name: 'Tony Stark', house_num: 12,
+                            street_name: 'W. Iron Rd.', city: 'New York city',
+                            state: nil, zip_code: 60_001)
+
     assert_not_nil_empty employee, :state_id
   end
 
   test 'state_id cannot be empty' do
-    employee = employees :employee_state_empty_val
-    employee.state_id = nil if employee.state_id.zero?
+    employee = Employee.new(name: 'Karl Stolley', house_num: 879,
+                            street_name: 'Bleek Blvd', city: 'Mt. prospect',
+                            state: State.new, zip_code: 60_645)
 
     assert_not_nil_empty employee, :state_id
   end
 
   test 'state_id does not exist but linked to employee' do
-    employee = employees :employee_state_not_exists
+    employee = Employee.new(name: 'Silver Surfer', house_num: 12,
+                            street_name: 'East Galaxy Rd.', city: 'Moon City',
+                            state_id: 302_572_571_204_975_052,
+                            zip_code: 10_000)
+
     assert employee.invalid?
     assert_not employee.errors[:state_id].any?
     assert employee.errors[:state_id], []
@@ -122,21 +173,35 @@ class EmployeeTest < ActiveSupport::TestCase
   end
 
   test 'zip code cannot be nil' do
-    employee = employees :employee_zip_code_nil_val
+    employee = Employee.new(name: 'Test Fail', house_num: 124,
+                            street_name: 'Kilburne Lane', city: 'Froth',
+                            state: (states :state_name_valid_1), zip_code: nil)
+
     assert_not_nil_empty employee, :zip_code
   end
 
   test 'zip code cannot be empty' do
-    employee = employees :employee_zip_code_empty_val
+    employee = Employee.new(name: 'Test Fail', house_num: 124,
+                            street_name: 'Kilburne Lane', city: 'Froth',
+                            state: (states :state_name_valid_0), zip_code: '')
+
     assert_not_nil_empty employee, :zip_code
   end
 
   test 'multiple invalid zip codes' do
     invalid_employees = [
-      employees(:employee_zip_code_invalid_0),
-      employees(:employee_zip_code_invalid_1),
-      employees(:employee_zip_code_invalid_2),
-      employees(:employee_zip_code_invalid_3)
+      Employee.new(name: 'Test Fail', house_num: 124,
+                   street_name: 'Kilburne Lane', city: 'Froth',
+                   state: (states :state_name_valid_1), zip_code: 1_871),
+      Employee.new(name: 'Test Fail', house_num: 124,
+                   street_name: 'Kilburne Lane', city: 'Froth',
+                   state: (states :state_name_valid_1), zip_code: 187_189),
+      Employee.new(name: 'Test Fail', house_num: 124,
+                   street_name: 'Kilburne Lane', city: 'Froth',
+                   state: (states :state_name_valid_1), zip_code: '19871code'),
+      Employee.new(name: 'Test Fail', house_num: 124,
+                   street_name: 'Kilburne Lane', city: 'Froth',
+                   state: (states :state_name_valid_1), zip_code: 'ninteen eighty one')
     ]
     invalid_employees.each do |employee|
       assert employee.invalid?
