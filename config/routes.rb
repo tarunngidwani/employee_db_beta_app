06661a716c35faaf80ec7a9857642eb8e1337001
nmares <nmares@hawk.iit.edu>
2017-06-25 20:08:18 -0500

    Add Session controller
      $rails g controller Session create destroy
    
    This will help create and login and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'session/create'
+
+  get 'session/destroy'
+
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
 
