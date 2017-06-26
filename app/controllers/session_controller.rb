b6647dc803826869f83334bcf5861bd5a1f8e742
nmares <nmares@hawk.iit.edu>
2017-06-25 21:33:42 -0500

    Modify session controller
    after user logs into app, user is rerouted to employee db.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,6 +2,13 @@ class SessionController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => :create
 
   def create
+    user = User.find_or_create_by(:provider => auth_hash[:provider], :uid => auth_hash[:uid]) do |user|
+      user.name = auth_hash[:info][:name]
+    end
+
+    session[:user_id] = user.id
+
+    redirect_to :employees
   end
 
   def destroy
