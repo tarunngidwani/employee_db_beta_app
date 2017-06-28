8a65c5f0d1c76195072bdfb4fc352970ca889e54
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-28 16:54:33 -0500

    Create function that generates current user hash
      - Add private method that creates a current user hash
        using the omniauth request hash returned from the provider
        in this case github once the user has successfully
        authenticated using said provider (github)

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,17 @@
 class LoginController < ApplicationController
 
   def login; end
+
+  private
+
+  def create_current_user_info_hash
+    github_auth_hash = request.env['omniauth.auth']
+    user_uid = github_auth_hash['uid']
+    user_name = github_auth_hash['info']['name']
+
+    current_user = {
+      uid: user_uid,
+      name: user_name
+    }
+  end
 end
