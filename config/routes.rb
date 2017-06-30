3246a4f451621bfa27fb0a87505ca1b729b1a96a
nmares <nmares@hawk.iit.edu>
2017-06-30 13:29:19 -0500

    Update session to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   root 'sessions#login'
 
   get 'auth/github', as: 'github_auth'
-  get 'auth/:provider/callback', to: 'session#create'
+  get 'auth/:provider/callback', to: 'sessions#create'
 
-  get 'session/destroy', as: 'logout'
+  get 'sessions/destroy', as: 'logout'
 
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
-
-
 
   resources :employees
 end
