3e1633d4c952443472cbbeeee3cec5c98a97dd83
nmares <nmares@hawk.iit.edu>
2017-06-30 13:34:27 -0500

    Clean up session controller and test
      - Remove unnecessary methods and actions
    
      - Remove non functioning test

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,25 +1,4 @@
 class SessionController < ApplicationController
-  skip_before_action :verify_authenticity_token, :only => :create
 
-  def create
-    user = User.find_or_create_by(:provider => auth_hash[:provider], :uid => auth_hash[:uid]) do |user|
-      user.name = auth_hash[:info][:name]
-    end
-
-    session[:user_id] = user.id
-
-    redirect_to :employees
-  end
-
-  def destroy
-    reset_session
-
-    redirect_to :root
-  end
-
-  private
-  def auth_hash
-    request.env["omniauth.auth"]
-  end
-
+  def login: end
 end
--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
-  # test "should get create" do
-  #   get github_auth_url
-  #   assert_response :success
-  # end
-
-  test "should get destroy" do
-    get root_url
-    assert_response :success
-  end
-
 end
