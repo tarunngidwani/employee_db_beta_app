6c08e5bbc425b9e6525665df7f588ce0cb0be51d
nmares <nmares@hawk.iit.edu>
2017-06-30 13:42:48 -0500

    Remove User model
      - Remove all files generated by rails g model generator
        related to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626020411) do
+ActiveRecord::Schema.define(version: 20170613180952) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 20170626020411) do
   create_table "states", force: :cascade do |t|
     t.string "name"
     t.string "name_abbr"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "uid"
-    t.string "provider"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
