1881472647bafce813366dbc00e9fa17e3900b23
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-07-02 17:26:35 -0500

    Add helper method in sessions controller tests
      - This method takes the value of the notice as an
        argument. Uses that value to assert that the notice
        in response body matches the notice passed in
    
      - Asserts that body of the response contains the
        appropriate html tags and their corresponding values
        on login page

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
+
+  private
+
+  def test_login_page_content(notice)
+    assert_select 'p', notice
+    assert_select 'h1', 'Employee Records Login'
+    assert_select 'a', 'Login with GitHub'
+  end
 end
