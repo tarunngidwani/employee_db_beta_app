8f5fc7eefc68ba7ccba5c657b86c12592793ceb4
nmares <nmares@hawk.iit.edu>
2017-07-02 15:33:05 -0500

    Remove methods from application controller
      - Move these methods to a controller concern for sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  helper_method :current_user, :logged_in?
-
-  def logged_in_user
-  end
-
-  def current_user
-    @current_user = session['current_user']
-  end
-
-  def logged_in?
-  end
 end
