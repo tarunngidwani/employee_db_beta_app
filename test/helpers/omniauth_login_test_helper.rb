3423ef3176c877550e0ad4246a8e948f9d77280c
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-07-02 13:57:45 -0500

    Create method that generates an OmniAuth AuthHash
      - This method creates and returns an OmniAuth AuthHash
        based on the provider and user name passed as arguments
        to it.
      - While testing, this is the hash that will get returned as
        we are mocking the ability to log in using omniauth through
        an available provider

--- test/helpers/omniauth_login_test_helper.rb
+++ test/helpers/omniauth_login_test_helper.rb
@@ -1,2 +1,8 @@
 module OmniauthLoginTestHelper
+  private
+
+  def create_omniauth_mock_auth_hash(provider, user_name)
+    OmniAuth::AuthHash.new(provider: provider, uid: 12_345,
+                           info: { name: user_name })
+  end
 end
