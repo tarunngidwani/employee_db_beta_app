3b8db119afa1f781ae76a0b0ff80794881beab30
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-07-02 14:05:57 -0500

    Create login method in OmniAuth Login Test Helper
          - The login method simulates the process of logging
            in using OmniAuth through a provider for testing
            purposes
          - It mocks the login process by bypassing the provider's
            login process and by returning the mock auth hash
            that we define
          - This auth hash that is used to mock a successful login
            through a provider using OmniAuth is set based on
            the provider and user name passed in as arguments
            to this method

--- test/helpers/omniauth_login_test_helper.rb
+++ test/helpers/omniauth_login_test_helper.rb
@@ -1,6 +1,13 @@
 module OmniauthLoginTestHelper
   private
 
+  def login(provider, user_name)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = create_omniauth_mock_auth_hash provider, user_name
+
+    get omniauth_callback_url(provider)
+  end
+
   def create_omniauth_mock_auth_hash(provider, user_name)
     OmniAuth::AuthHash.new(provider: provider, uid: 12_345,
                            info: { name: user_name })
