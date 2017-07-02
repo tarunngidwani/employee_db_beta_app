aab5bc601b565991a7bbc029979f6ba5d2b9187f
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-07-02 17:38:09 -0500

    Add should get login test for sessions controller
      - This test ensures that we are able to get to
        the login page by asserting we get a 200 success
        response on doing so
    
      - We also call the helper method to verify that we get
        the appropriate html response when visiting the
        login_url

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
 
+  test 'should get login' do
+    get login_url
+    assert_response :success
+
+    notice = ''
+    test_login_page_content notice
+  end
+
   private
 
   def test_login_page_content(notice)
